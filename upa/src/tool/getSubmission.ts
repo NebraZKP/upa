0fa7ab010e7f7950793437660660779e71273ae8
Duncan Tebbs <dtebbs@users.noreply.github.com>
2025-04-02 14:59:51 +0100

    feat[upa]: parse events from tx receipt. --events flag on get-submission

--- upa/src/tool/getSubmission.ts
+++ upa/src/tool/getSubmission.ts
@@ -1,9 +1,12 @@
-import { command, positional, string } from "cmd-ts";
+import { command, flag, positional, string } from "cmd-ts";
 import * as options from "./options";
 import * as ethers from "ethers";
 import * as config from "./config";
 import * as utils from "../sdk/utils";
 import { Submission } from "../sdk";
+import { EventSet, ProofSubmittedEventGetter } from "../sdk/events";
+// eslint-disable-next-line
+import { ProofSubmittedEvent } from "../../typechain-types/contracts/tests/TestUpgradedUpaVerifier";
 
 export const getSubmission = command({
   name: "get-submission",
@@ -15,9 +18,18 @@ export const getSubmission = command({
       displayName: "tx-id",
       description: "Tx Id of the submission to retrieve",
     }),
+    events: flag({
+      long: "events",
+      description: "Dump event data for the submission",
+    }),
   },
   description: "Get the on-chain submission information associated with a Tx",
-  handler: async function ({ chainEndpoint, instance, txId }): Promise<void> {
+  handler: async function ({
+    chainEndpoint,
+    instance,
+    txId,
+    events,
+  }): Promise<void> {
     const provider = new ethers.JsonRpcProvider(chainEndpoint);
     const { verifier } = await config.upaFromInstanceFile(instance, provider);
 
@@ -29,6 +41,19 @@ export const getSubmission = command({
       verifier,
       txReceipt
     );
+
+    // If the events flag is given, dump the event data.
+    if (events) {
+      const evGetter = new ProofSubmittedEventGetter(verifier);
+      const eventSet = evGetter.parseTransactionReceipt(txReceipt);
+      // Put the event set on an `eventSet` attribute of the submission
+      // object.
+      (
+        submission as unknown as {
+          eventSet: EventSet<ProofSubmittedEvent.OutputObject>;
+        }
+      ).eventSet = eventSet;
+    }
 
     console.log(utils.JSONstringify(submission));
   },
